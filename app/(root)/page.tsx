245b1c76b1cddfd932b2f00a2a2a0690f0c7d359
mhmd-aashik <aashikdevelop@gmail.com>
2024-04-14 14:06:28 +0530

    update 2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { getAllUsers } from "@/lib/actions/user.action";
+import { createUser, getAllUsers } from "@/lib/actions/user.action";
 import Image from "next/image";
 import Link from "next/link";
 
--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/events/:id", "/api/webhook/clerk"],
-  ignoredRoutes: ["/api/webhook/clerk"],
+  publicRoutes: ["/", "/events/:id", "/api/webhooks/clerk"],
+  ignoredRoutes: ["/api/webhooks/clerk"],
 });
 
 export const config = {
